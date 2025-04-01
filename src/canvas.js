52c84c8aee85da304876f017b44bad35049ba0c1
Justin Lefkowitz <justinmlefkowitz@gmail.com>
2025-04-01 16:31:34 -0600

    objectified code (haha sounds weird)

--- src/canvas.js
+++ src/canvas.js
@@ -1,149 +1,275 @@
+//initialize canvas
 const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('dice'))
 const ctx = canvas.getContext("2d");
 
 
 
-
+//set variables
 const rows = 40;
-const columns = 40;
+const columns = 80;
 const width = canvas.width / columns;
 const height = canvas.height / rows;
-const radius = 2.5;
-const padding = 5;
+const radius = 2.5 ;
+const padding = 5 ;
 
 const backgroundColor = "rgb(222,199,160)";
 
-const shadowOffset = 1.4;
-const shadowColor = "black"
+const shadowOffset = 1;
+const shadowColor = "black";
 
-var dice = Array.from({ length: rows }, () => Array.from({ length: columns }, () => 0)); // Initialize with 0;
+const drawBorders = false;
 
-for (let i = 0; i < columns; i++) {
-    for (let j = 0; j < rows; j++) {
-        if (dice[i][j] == 0) {
-            if (Math.random() > .9 && i + 1 < columns && j + 1 < rows && dice[i][j+1] == 0) {
-                dice[i][j] = Math.round(6 * Math.random() + .5) + 6;
-                dice[i][j+1] = -1;
-                dice[i+1][j] = -2;
-                dice[i+1][j+1] = -3;
+var dice;
+
+
+//Dice class with constructor, draw and roll methods
+class Dice {
+
+    constructor(n, x, y, size, padding, radius, rotate, color, shadowOffset, shadowColor, isBorder) {
+        this.n = n;
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.radius = radius * size;
+        this.padding = padding * size;
+        this.rotate = rotate;
+        this.color = color;  
+        this.shadowOffset = shadowOffset * size;
+        this.shadowColor = shadowColor;
+        this.isBorder = isBorder;
+        this.width = width * size;
+        this.height = height * size;
+
+    }
+
+    drawCircle(x,y,r) {
+        ctx.beginPath()
+        ctx.arc(x,y,r,0,2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    drawShadow() {
+        ctx.fillStyle = this.shadowColor;
+
+        if (this.n == 1) {
+
+            this.drawCircle(this.x + this.width/2 + this.shadowOffset, this.y + this.height/2 + this.shadowOffset, this.radius);
+
+        } else if (this.n == 2) {
+
+            if (this.rotate == true) {
+                this.drawCircle(this.x + this.padding + this.shadowOffset, this.y + this.padding + this.shadowOffset, this.radius);
+                this.drawCircle(this.x + this.width - this.padding + this.shadowOffset, this.y + this.height - this.padding + this.shadowOffset, this.radius);
             } else {
-                dice[i][j] = Math.round(6 * Math.random() + .5);
+                this.drawCircle(this.x + this.padding + this.shadowOffset, this.y + this.height - this.padding + this.shadowOffset, this.radius);
+                this.drawCircle(this.x + this.width - this.padding + this.shadowOffset, this.y + this.padding + this.shadowOffset, this.radius);
             }
-        }  
+            
+        } else if (this.n == 3) {
+
+            this.drawCircle(this.x + this.width/2 + this.shadowOffset, this.y + this.height/2 + this.shadowOffset, this.radius);
+
+            if (this.rotate == true) {
+                this.drawCircle(this.x + this.padding + this.shadowOffset, this.y + this.padding + this.shadowOffset, this.radius);
+                this.drawCircle(this.x + this.width - this.padding + this.shadowOffset, this.y + this.height - this.padding + this.shadowOffset, this.radius);
+            } else {
+                this.drawCircle(this.x + this.padding + this.shadowOffset, this.y + this.height - this.padding + this.shadowOffset, this.radius);
+                this.drawCircle(this.x + this.width - this.padding + this.shadowOffset, this.y + this.padding + this.shadowOffset, this.radius);
+            }
+            
+        } else if (this.n == 4) {
+
+            this.drawCircle(this.x + this.padding + this.shadowOffset, this.y + this.padding + this.shadowOffset, this.radius);
+            this.drawCircle(this.x + this.width - this.padding + this.shadowOffset, this.y + this.height - this.padding + this.shadowOffset, this.radius);
+            this.drawCircle(this.x + this.padding + this.shadowOffset, this.y + this.height - this.padding + this.shadowOffset, this.radius);
+            this.drawCircle(this.x + this.width - this.padding + this.shadowOffset, this.y + this.padding + this.shadowOffset, this.radius);
+
+        } else if (this.n == 5) {
+            
+            this.drawCircle(this.x + this.width/2 + this.shadowOffset, this.y + this.height/2 + this.shadowOffset, this.radius);
+            this.drawCircle(this.x + this.padding + this.shadowOffset, this.y + this.padding + this.shadowOffset, this.radius);
+            this.drawCircle(this.x + this.width - this.padding + this.shadowOffset, this.y + this.height - this.padding + this.shadowOffset, this.radius);
+            this.drawCircle(this.x + this.padding + this.shadowOffset, this.y + this.height - this.padding + this.shadowOffset, this.radius);
+            this.drawCircle(this.x + this.width - this.padding + this.shadowOffset, this.y + this.padding + this.shadowOffset, this.radius);
+            
+        } else if (this.n == 6) {
+
+            this.drawCircle(this.x + this.padding + this.shadowOffset, this.y + this.padding + this.shadowOffset, this.radius);
+            this.drawCircle(this.x + this.width - this.padding + this.shadowOffset, this.y + this.height - this.padding + this.shadowOffset, this.radius);
+            this.drawCircle(this.x + this.padding + this.shadowOffset, this.y + this.height - this.padding + this.shadowOffset, this.radius);
+            this.drawCircle(this.x + this.width - this.padding + this.shadowOffset, this.y + this.padding + this.shadowOffset, this.radius);
+
+            if (this.rotate == true) {
+                this.drawCircle(this.x + this.width/2 + this.shadowOffset, this.y + this.padding + this.shadowOffset, this.radius);
+                this.drawCircle(this.x + this.width/2 + this.shadowOffset, this.y + this.height - this.padding + this.shadowOffset, this.radius);
+            } else {
+                this.drawCircle(this.x + this.padding + this.shadowOffset, this.y + this.height/2 + this.shadowOffset, this.radius);
+                this.drawCircle(this.x + this.width - this.padding + this.shadowOffset, this.y + this.height/2 + this.shadowOffset, this.radius);
+            }
+        }
+    }
+    drawDice() {
+
+        ctx.fillStyle = backgroundColor;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        if (this.isBorder) {
+            ctx.strokeStyle = "black";
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+        }
+        
+        this.drawShadow()
+        //color setting
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = this.color;
+
+        //dice value drawing
+        if (this.n == 1) {
+
+            this.drawCircle(this.x + this.width/2, this.y + this.height/2, this.radius);
+
+        } else if (this.n == 2) {
+
+            if (this.rotate == true) {
+                this.drawCircle(this.x + this.padding, this.y + this.padding, this.radius);
+                this.drawCircle(this.x + this.width - this.padding, this.y + this.height - this.padding, this.radius);
+            } else {
+                this.drawCircle(this.x + this.padding, this.y + this.height - this.padding, this.radius);
+                this.drawCircle(this.x + this.width - this.padding, this.y + this.padding, this.radius);
+            }
+            
+        } else if (this.n == 3) {
+
+            this.drawCircle(this.x + this.width/2, this.y + this.height/2, this.radius);
+
+            if (this.rotate == true) {
+                this.drawCircle(this.x + this.padding, this.y + this.padding, this.radius);
+                this.drawCircle(this.x + this.width - this.padding, this.y + this.height - this.padding, this.radius);
+            } else {
+                this.drawCircle(this.x + this.padding, this.y + this.height - this.padding, this.radius);
+                this.drawCircle(this.x + this.width - this.padding, this.y + this.padding, this.radius);
+            }
+            
+        } else if (this.n == 4) {
+
+            this.drawCircle(this.x + this.padding, this.y + this.padding, this.radius);
+            this.drawCircle(this.x + this.width - this.padding, this.y + this.height - this.padding, this.radius);
+            this.drawCircle(this.x + this.padding, this.y + this.height - this.padding, this.radius);
+            this.drawCircle(this.x + this.width - this.padding, this.y + this.padding, this.radius);
+
+        } else if (this.n == 5) {
+            
+            this.drawCircle(this.x + this.width/2, this.y + this.height/2, this.radius);
+            this.drawCircle(this.x + this.padding, this.y + this.padding, this.radius);
+            this.drawCircle(this.x + this.width - this.padding, this.y + this.height - this.padding, this.radius);
+            this.drawCircle(this.x + this.padding, this.y + this.height - this.padding, this.radius);
+            this.drawCircle(this.x + this.width - this.padding, this.y + this.padding, this.radius);
+            
+        } else if (this.n == 6) {
+
+            this.drawCircle(this.x + this.padding, this.y + this.padding, this.radius);
+            this.drawCircle(this.x + this.width - this.padding, this.y + this.height - this.padding, this.radius);
+            this.drawCircle(this.x + this.padding, this.y + this.height - this.padding, this.radius);
+            this.drawCircle(this.x + this.width - this.padding, this.y + this.padding, this.radius);
+
+            if (this.rotate == true) {
+                this.drawCircle(this.x + this.width/2, this.y + this.padding, this.radius);
+                this.drawCircle(this.x + this.width/2, this.y + this.height - this.padding, this.radius);
+            } else {
+                this.drawCircle(this.x + this.padding, this.y + this.height/2, this.radius);
+                this.drawCircle(this.x + this.width - this.padding, this.y + this.height/2, this.radius);
+            }
+        }
+
+    }
+
+    rollDice() {
+            
+        if (this.n <= 6 && this.n > 0) {
+            this.n++;
+            if (this.n == 7) {
+                this.n = 1;
+            }
+            console.log("Dice at (" + Math.floor(this.x/this.width) + ", " + Math.floor(this.y/this.height) +") incremented");
+            this.drawDice(ctx);
+        } else {
+            console.log("oopsies dice out of bounds here");
+        }
     }
 }
 
+//mouse click listener to roll selected dice
 canvas.addEventListener("click", function (e) {
-    const x = e.offsetX;
-    const y = e.offsetY;
+    let a = 0;
+    let b = 0;
 
-    console.log("Clicked at (" + x + ", " + y + ")");
-    rollDice(Math.floor(x/width), Math.floor(y/height));
+
+    const i = Math.floor(e.offsetX/width)
+    const j = Math.floor(e.offsetY/height);
+    if (dice[i][j] == -1) {
+        a = 1;
+    } else if (dice[i][j] == -2) {
+        b = 1;
+    } else if (dice[i][j] == -3) {
+        a = 1;
+        b = 1;
+    } 
+
+    dice[i-b][j-a].rollDice();
+
+
+    
 });
 
-
-
-
-function drawCircle(x,y,r) {
-    ctx.beginPath()
-    ctx.arc(x,y,r,0,2 * Math.PI);
-    ctx.fill();
-    ctx.stroke();
-}
-
-function drawDice(n, x, y, width, length, padding, radius, rotate, color) {
-
-    //outline border
-    //ctx.strokeStyle = "black";
-    //ctx.strokeRect(x,y, width, length);
-
     
+//sets dice matrix
+function initialize() {
+    dice = []
 
-    //color setting
-    ctx.fillStyle = color;
-    ctx.strokeStyle = color;
+    for (let i = 0; i < columns; i++) {
+        dice[i] = [];
+        for (let j = 0; j < rows; j++) {
 
-    //dice value drawing
-    if (n == 1) {
+            dice[i][j] = 0;
 
-        drawCircle(x + width/2, y + length/2, radius, color);
-
-    } else if (n == 2) {
-
-        if (rotate == true) {
-            drawCircle(x + padding, y + padding, radius);
-            drawCircle(x + width - padding, y + length - padding, radius);
-        } else {
-            drawCircle(x + padding, y + length - padding, radius);
-            drawCircle(x + width - padding, y + padding, radius);
-        }
-        
-    } else if (n == 3) {
-
-        drawCircle(x + width/2, y + length/2, radius, color);
-
-        if (rotate == true) {
-            drawCircle(x + padding, y + padding, radius);
-            drawCircle(x + width - padding, y + length - padding, radius);
-        } else {
-            drawCircle(x + padding, y + length - padding, radius);
-            drawCircle(x + width - padding, y + padding, radius);
-        }
-        
-    } else if (n == 4) {
-
-        drawCircle(x + padding, y + padding, radius);
-        drawCircle(x + width - padding, y + length - padding, radius);
-        drawCircle(x + padding, y + length - padding, radius);
-        drawCircle(x + width - padding, y + padding, radius);
-
-    } else if (n == 5) {
-        
-        drawCircle(x + width/2, y + length/2, radius, color);
-        drawCircle(x + padding, y + padding, radius);
-        drawCircle(x + width - padding, y + length - padding, radius);
-        drawCircle(x + padding, y + length - padding, radius);
-        drawCircle(x + width - padding, y + padding, radius);
-        
-    } else if (n == 6) {
-
-        drawCircle(x + padding, y + padding, radius);
-        drawCircle(x + width - padding, y + length - padding, radius);
-        drawCircle(x + padding, y + length - padding, radius);
-        drawCircle(x + width - padding, y + padding, radius);
-
-        if (rotate == true) {
-            drawCircle(x + width/2, y + padding, radius);
-            drawCircle(x + width/2, y + length - padding, radius);
-        } else {
-            drawCircle(x + padding, y + length/2, radius);
-            drawCircle(x + width - padding, y + length/2, radius);
-        }
+        } 
     }
 
-
-}
-
-function rollDice(i, j) {
-    
-    if (dice[i][j] <= 6 && dice[i][j] > 0) {
-        dice[i][j]++;
-        if (dice[i][j] == 7) {
-            dice[i][j] = 1;
+    for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+            if (dice[i][j] != -1 && dice[i][j] != -2 && dice[i][j] != -3) {
+                if (Math.random() > .92 && i + 1 < columns && j + 1 < rows && dice[i][j+1] == 0) {
+                    dice[i][j] = new Dice(Math.round(6 * Math.random() + .5), i*width, j*height, 2, padding, radius, Math.round(Math.random()), `rgb( ${87 + (168 * ((i + j)/(columns + rows)))},${12 + (243 * ((i + j)/(columns + rows)))},${160 - (160 * ((i + j)/(columns + rows)))})`, shadowOffset, shadowColor, drawBorders);
+                    dice[i][j+1] = -1;
+                    dice[i+1][j] = -2;
+                    dice[i+1][j+1] = -3;
+                } else {
+                    dice[i][j] = new Dice(Math.round(6 * Math.random() + .5), i*width, j*height, 1, padding, radius, Math.round(Math.random()), `rgb( ${87 + (168 * ((i + j)/(columns + rows)))},${12 + (243 * ((i + j)/(columns + rows)))},${160 - (160 * ((i + j)/(columns + rows)))})`, shadowOffset, shadowColor, drawBorders);
+                }
+            }
+            
         }
-        console.log("Dice at (" + i + ", " + j +") incremented");
-        renderRoll(i,j);
+        
     }
-    
+    console.log(dice);
 }
 
+//renders entire dice matrix
 function render() {
 
     ctx.fillStyle = backgroundColor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < columns; i++) {
         for (let j = 0; j < rows; j++) {
-    
+
+            if (dice[i][j] != -1 && dice[i][j] != -2 && dice[i][j] != -3) { 
+                dice[i][j].drawDice();
+            }
+            
+            
+            
+            /*
             let r = Math.round(Math.random());
             let color = `rgb( ${87 + (168 * ((i + j)/(columns + rows)))},${12 + (243 * ((i + j)/(columns + rows)))},${160 - (160 * ((i + j)/(columns + rows)))})`;
             
@@ -154,22 +280,16 @@ function render() {
                 drawDice(dice[i][j], i*width + shadowOffset, j*height + shadowOffset, width, height, padding, radius, r, shadowColor);
                 drawDice(dice[i][j], i*width, j*height, width, height, padding, radius, r, color);
             }
+            */
+
+            
             
         }
     }
 }
 
-function renderRoll(i, j) {
-    ctx.fillStyle = backgroundColor;
-    ctx.fillRect(i*width, j*height, width, height)
-
-    let r = Math.round(Math.random());
-    let color = `rgb( ${87 + (168 * ((i + j)/(columns + rows)))},${12 + (243 * ((i + j)/(columns + rows)))},${160 - (160 * ((i + j)/(columns + rows)))})`;
-    
-
-    drawDice(dice[i][j], i*width + shadowOffset, j*height + shadowOffset, width, height, padding, radius, r, shadowColor);
-    drawDice(dice[i][j], i*width, j*height, width, height, padding, radius, r, color);
-}
 
 
+initialize();
 render()
+
